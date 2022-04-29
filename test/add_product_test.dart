3fd50a50899b6dc105bdcbc6fe25ce5e0199b1f9
ildysilva <ildeberto34@gmail.com>
2022-04-29 11:41:18 +0100

    App Working

---
+++ test/add_product_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:cubotest/main.dart';
+
+void main() {
+  test('Counter increments smoke test', () async {
+    // Build our app and trigger a frame.
+
+
+
+
+    expect(find.byType(int) findsNothing);
+  });
+}
